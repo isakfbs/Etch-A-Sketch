6abe01e880476423a32d737ed2b1cd6b6973d92d
Isak Silva <isakfreires98@gmail.com>
2022-12-12 19:02:26 -0300

    Make a button that change the grid size

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
 const container = document.querySelector('.grid-container');
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
+let rows = document.getElementsByClassName('gridRow');
+let cells = document.getElementsByClassName('cell');
+let gridSizeButton = document.getElementById('gridSize-button');
+
+//Drawing functions
 
 let isDrawing = false;
 
+function draw(e, cell) {
+  if (!isDrawing) return;
+  cell.classList.add('cellColor');
+  console.log(e);
+}
+//Grid maker
 
-function draw(e,cell){
-    if(!isDrawing) return;
-    cell.classList.add("cellColor");
-    console.log(e);
-};
-
-function makeGrid(size){
-    //Creates rows
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    for (let i = 0; i < size * size; i++) {
-        let cell = document.createElement('div');
-        cell.setAttribute('data-cell',`cell${i}`);
-        cell.addEventListener('mousemove', function(e){draw(e,this)});
-        cell.addEventListener('mousedown', () => isDrawing = true);
-        cell.addEventListener('mouseup', () => isDrawing = false);
-        container.appendChild(cell);
-    }
+function makeGrid(size) {
+  //Creates rows
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  for (let i = 0; i < size * size; i++) {
+    let cell = document.createElement('div');
+    cell.setAttribute('data-cell', `cell${i}`);
+    cell.addEventListener('mousemove', function (e) {
+      draw(e, this);
+    });
+    cell.addEventListener('mousedown', () => (isDrawing = true));
+    cell.addEventListener('mouseup', () => (isDrawing = false));
+    container.appendChild(cell);
+  }
 }
 
-// function makeColumns(cellNum){
-//     for(let i = 0; i < rows.length; i++){
-//         for(j = 0; j < cellNum; j++){
-//             let newCell = document.createElement("div");
-//             rows[j].appendChild(newCell).className = "cell";
-//         }
-//     }
-// }
-
 makeGrid(16);
-// makeColumns(5);
+
+//Button panel
+
+gridSizeButton.addEventListener('click', function () {
+  let size = prompt('Grid Size');
+  if (size <= 100) {
+    makeGrid(+size);
+  } else {
+    size = prompt('Invalid Size, Try a number below 100px');
+    makeGrid(+size);
+  }
+});
